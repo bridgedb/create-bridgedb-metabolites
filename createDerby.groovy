0efc28430e5e15e2799b914e459cdb5c8d4085f5
DeniseSl22 <denise.slenter@maastrichtuniversity.nl>
2020-10-02 15:00:02 +0200

    Including mesh mapping terms

--- createDerby.groovy
+++ createDerby.groovy
@@ -497,6 +497,36 @@ new File("gpl2wikidata.csv").eachLine { line,number ->
 }
 unitReport << "  <testcase classname=\"WikidataCreation\" name=\"IUPHARFound\"/>\n"
 
+// MeSH Descriptor IDs
+counter = 0
+error = 0
+new File("mesh2wikidata.csv").eachLine { line,number ->
+  if (number == 1) return // skip the first line
+
+  fields = line.split(",")
+  rootid = fields[0].substring(31)
+  Xref ref = new Xref(rootid, wikidataDS);
+  if (!genesDone.contains(ref.toString())) {
+    addError = database.addGene(ref);
+    if (addError != 0) println "Error (addGene): " + database.recentException().getMessage()
+    error += addError
+    linkError = database.addLink(ref,ref);
+    if (linkError != 0) println "Error (addLinkItself): " + database.recentException().getMessage()
+    error += linkError
+    genesDone.add(ref.toString())
+  }
+
+  // add external identifiers
+  addXRef(database, ref, fields[1], meshDS, genesDone, linksDone);
+
+  counter++
+  if (counter % commitInterval == 0) {
+    println "Info: errors: " + error + " (MeSH)"
+    database.commit()
+  }
+}
+
+
 //// VMH Metabolite identifiers
 //counter = 0
 //error = 0
